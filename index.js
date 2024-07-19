4f8650f3c91df7385937c4ee9e00a9755aff760e
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-07-19 18:58:55 +0200

    Set cell attributes (color, background, decorations, align)

--- index.js
+++ index.js
@@ -1,3 +1,76 @@
+const codesANSI = {
+    reset: "\u001b[0m",
+    foreground: {
+        black: "\u001b[30m",
+        red: "\u001b[31m",
+        green: "\u001b[32m",
+        yellow: "\u001b[33m",
+        blue: "\u001b[34m",
+        magenta: "\u001b[35m",
+        cyan: "\u001b[36m",
+        white: "\u001b[37m",
+        gray: "\u001b[90m",
+        brightRed: "\u001b[91m",
+        brightGreen: "\u001b[92m",
+        brightYellow: "\u001b[93m",
+        brightBlue: "\u001b[94m",
+        brightMagenta: "\u001b[95m",
+        brightCyan: "\u001b[96m",
+        brightWhite: "\u001b[97m",
+    },
+    background: {
+        black: "\u001b[40m",
+        red: "\u001b[41m",
+        green: "\u001b[42m",
+        yellow: "\u001b[43m",
+        blue: "\u001b[44m",
+        magenta: "\u001b[45m",
+        cyan: "\u001b[46m",
+        white: "\u001b[47m",
+        gray: "\u001b[100m",
+        brightRed: "\u001b[101m",
+        brightGreen: "\u001b[102m",
+        brightYellow: "\u001b[103m",
+        brightBlue: "\u001b[104m",
+        brightMagenta: "\u001b[105m",
+        brightCyan: "\u001b[106m",
+        brightWhite: "\u001b[107m",
+    },
+    decorations: {
+        bold: "\u001b[1m",
+        dim: "\u001b[2m",
+        italic: "\u001b[3m",
+        underline: "\u001b[4m",
+        overline: "\u001b[53m",
+        inverse: "\u001b[7m",
+        strikethrough: "\u001b[9m",
+        slowBlink: "\u001b[5m",
+        rapidBlink: "\u001b[6m",
+    },
+};
+
+function getCellText(content, colWidth, cellAttrs) {
+    var attrs;
+    if (typeof cellAttrs === "object") attrs = cellAttrs;
+    else attrs = {};
+
+    var columnStr = "";
+    var colorANSI = "";
+    var backgroundANSI = "";
+    var decorationsANSI = "";
+    var resetANSI = "";
+
+    if (typeof attrs.color === "string") colorANSI = codesANSI.foreground[attrs.color];
+    if (typeof attrs.bgColor === "string") backgroundANSI = codesANSI.background[attrs.bgColor];
+    if (Array.isArray(attrs.decorations) && attrs.decorations.length > 0) attrs.decorations.forEach((decoration) => { decorationsANSI += codesANSI.decorations[decoration]; });
+    if (typeof attrs.color === "string" || typeof attrs.bgColor === "string" || (Array.isArray(attrs.decorations) && attrs.decorations.length > 0)) resetANSI = codesANSI.reset;
+
+    if (attrs.align === "left" || typeof attrs.align === "undefined") columnStr += colorANSI + backgroundANSI + decorationsANSI + content + resetANSI + " ".repeat(colWidth - content.length);
+    else if (attrs.align === "center") columnStr += " ".repeat(Math.ceil((colWidth - content.length) / 2)) + colorANSI + backgroundANSI + decorationsANSI + content + resetANSI + " ".repeat(Math.floor((colWidth - content.length) / 2));
+    else if (attrs.align === "right") columnStr += " ".repeat(colWidth - content.length) + colorANSI + backgroundANSI + decorationsANSI + content + resetANSI;
+    return columnStr;
+}
+
 class Table {
     constructor(options) {
         if (typeof options === "undefined") {
@@ -133,6 +206,7 @@ class Table {
         }
 
         this.rows = [];
+        this.attrs = {};
     }
 
     insertRow(row) {
@@ -190,7 +264,7 @@ class Table {
         if (table.length > 0) {
             stringTable += "\n";
             for (var i = 0; i < table[0].length; i++) {
-                stringTable += this.borders.sep + " ".repeat(this.leftPadding) + table[0][i] + " ".repeat(columnsLengths[i] - table[0][i].length) + " ".repeat(this.rightPadding);
+                stringTable += this.borders.sep + " ".repeat(this.leftPadding) + getCellText(table[0][i], columnsLengths[i], this.attrs[`cell:${0},${i}`]) + " ".repeat(this.rightPadding);
             }
             stringTable += this.borders.sep;
         }
@@ -207,7 +281,7 @@ class Table {
                 if (table[i] !== "HORIZONTAL LINE") {
                     stringTable += "\n";
                     for (var j = 0; j < table[i].length; j++) {
-                        stringTable += this.borders.sep + " ".repeat(this.leftPadding) + table[i][j] + " ".repeat(columnsLengths[j] - table[i][j].length) + " ".repeat(this.rightPadding);
+                        stringTable += this.borders.sep + " ".repeat(this.leftPadding) + getCellText(table[i][j], columnsLengths[j], this.attrs[`cell:${i},${j}`]) + " ".repeat(this.rightPadding);
                     }
                     stringTable += this.borders.sep;
                     if (this.horizontalLines && i < table.length - 1) {
@@ -241,7 +315,7 @@ class Table {
         this.rows.push("HORIZONTAL LINE");
     }
 
-    getCell(col, row) {
+    getCell(row, col) {
         if (row >= this.rows.length) throw RangeError("row number out of range");
         if (this.rows[row] === "HORIZONTAL LINE") throw SyntaxError("this row is horizontal line");
         var rowContent = this.getRows()[row];
@@ -249,12 +323,31 @@ class Table {
         return rowContent[col];
     }
 
-    setCell(col, row, value) {
+    setCell(row, col, value) {
         if (row >= this.rows.length) throw RangeError("row number out of range");
         if (this.rows[row] === "HORIZONTAL LINE") throw SyntaxError("this row is horizontal line");
         var rowContent = this.getRows()[row];
         if (col >= rowContent.length) throw RangeError("column number out of range");
         this.rows[row][col] = String(value);
+    }
+
+    setCellAttrs(row, col, attrs) {
+        if (row >= this.rows.length) throw RangeError("row number out of range");
+        if (this.rows[row] === "HORIZONTAL LINE") throw SyntaxError("this row is horizontal line");
+        var rowContent = this.getRows()[row];
+        if (col >= rowContent.length) throw RangeError("column number out of range");
+        if (typeof attrs === "undefined") delete this.attrs[`cell:${row},${col}`];
+        else if (typeof attrs === "object") {
+            Object.keys(attrs).forEach((key) => { if (!["color", "bgColor", "decorations", "align"].includes(key)) throw SyntaxError(`invalid attribute "${key}"`); });
+            if (typeof attrs.color !== "string" && typeof attrs.color !== "undefined") throw TypeError("attrs.color must be a string or undefined");
+            if (typeof attrs.bgColor !== "string" && typeof attrs.bgColor !== "undefined") throw TypeError("attrs.bgColor must be a string or undefined");
+            if (!Array.isArray(attrs.decorations) && typeof attrs.decorations !== "undefined") throw TypeError("attrs.decorations must be an array or undefined");
+            if (typeof attrs.color === "string" && !codesANSI.foreground.hasOwnProperty(attrs.color)) throw SyntaxError("attrs.color is invalid");
+            if (typeof attrs.bgColor === "string" && !codesANSI.background.hasOwnProperty(attrs.bgColor)) throw SyntaxError("attrs.bgColor is invalid");
+            if (Array.isArray(attrs.decorations)) attrs.decorations.forEach((name) => { if (!Object.keys(codesANSI.decorations).includes(name)) throw SyntaxError(`invalid decoration "${name}"`); });
+            if (typeof attrs.align !== "undefined" && attrs.align !== "left" && attrs.align !== "center" && attrs.align !== "right") throw SyntaxError('attrs.align must be "left", "center", "right" or undefined');
+            this.attrs[`cell:${row},${col}`] = attrs;
+        } else throw TypeError("attrs must be an object or undefined");
     }
 }
 
